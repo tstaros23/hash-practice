94c37fea977d2cf1b2fc7b3c052f3762af4ae66d
Ted Staros <tstaros23@gmail.com>
2021-04-01 15:40:14 -0600

    Add hash variables

--- hash-practice.rb
+++ hash-practice.rb
@@ -1,1 +1,11 @@
-car = {}
+car = {
+  "maker" => 'Honda',
+  "model" => 'CR-V',
+}
+
+
+animal = {
+  "elephant" => 'Dante Elefante',
+  "favorite_food" => 'peanuts',
+  "his_height_in_feet" => 10,
+}
